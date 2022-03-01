60ce0067ef9aeceddf1d65f546fc8df07841359c
Danny <64541739+Tho100@users.noreply.github.com>
2022-03-01 13:17:28 +0800

    Update Population.py

--- Population.py
+++ Population.py
@@ -1,3 +1,11 @@
+import cmath
+
+ar = [3,5,7]
+# Find ar mean
+sum_ar = 0
+for p in range(len(ar)):
+    sum_ar = sum_ar + ar[p]
+
 mu_mean = (sum_ar/len(ar))
 
 # Iterate each ar value with mu_mean and squared each of them
